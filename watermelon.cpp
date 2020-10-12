05c6464d0ef7fc97c263d4abb5d53828e3123fa6
Jasper Schol <jasper.schol@live.nl>
2020-10-12 15:29:30 +0200

    watermelon.cpp

---
+++ watermelon.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+// #include <boost>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+
+		int w;
+
+		cin >> w;
+		// ternary operator
+		// variable = (condition) ? expressionTrue : expressionFalse;
+		cout << (w % 2 == 0 && w != 2 ? "YES" : "NO");
+
+		// shorthand 
+		// if (w % 2 == 0 && w != 2) cout << "YES";
+		// else cout << "NO";
+
+		// normal 
+		// if (w % 2 == 0 && w != 2) {
+		// 	cout << "YES";
+		// }
+		// else{
+		// 	cout << "NO";
+		// }
+
+		return 0;
+}
